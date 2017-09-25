f6a785427be1326ecc5bca249fe3192fdc804206
Simon Shaw <SJShaw@users.noreply.github.com>
2017-09-25 15:24:23 +0200

    main: allow --list-plugins and --check-prereqs to function properly

--- antismash/test/test_antismash.py
+++ antismash/test/test_antismash.py
@@ -3,24 +3,30 @@
 
 import unittest
 
-from antismash.main import verify_options, get_detection_modules, get_analysis_modules
+from antismash import main
 from antismash.config.args import build_parser
 
 class TestAntismash(unittest.TestCase):
     def setUp(self):
-        self.all_modules = get_detection_modules() + get_analysis_modules()
+        self.all_modules = main.get_all_modules()
         self.default_options = build_parser(modules=self.all_modules).parse_args([])
 
     def test_default_options(self):
         # default options should work with all normal modules
         options = self.default_options
-        assert verify_options(options, self.all_modules)
+        assert main.verify_options(options, self.all_modules)
 
         # adding an incompatibility should not be ok
         options.tta = True
         options.input_type = 'prot'
-        assert not verify_options(options, self.all_modules)
+        assert not main.verify_options(options, self.all_modules)
 
         # and just to be sure the above isn't just because tta
         options.input_type = 'nucl'
-        assert verify_options(options, self.all_modules)
+        assert main.verify_options(options, self.all_modules)
+
+    def test_help_options(self):
+        for option in ["--list-plugins", "--check-prereqs"]:
+            options = build_parser(modules=self.all_modules).parse_args([option])
+            ret_val = main.run_antismash("", options)
+            assert ret_val == 0
