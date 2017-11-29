e14c31dbce2ff01d23d1587328c74ff458a6a217
Simon Shaw <SJShaw@users.noreply.github.com>
2017-11-29 15:20:35 +0100

    genefinding: improve integration testing

--- antismash/detection/genefinding/test/integration_prodigal.py
+++ antismash/detection/genefinding/test/integration_prodigal.py
@@ -4,34 +4,27 @@
 # for test files, silence irrelevant and noisy pylint warnings
 # pylint: disable=no-self-use,protected-access,missing-docstring
 
-import os
 from unittest import TestCase
 
 from antismash.config import get_config, update_config
 from antismash.common.record_processing import parse_input_sequence, pre_process_sequences
-from antismash.common.path import get_full_path
-from antismash.common.test.helpers import get_simple_options
+from antismash.common.test.helpers import get_simple_options, get_path_to_nisin_fasta
 from antismash.detection import genefinding
 
 
-class TestGlimmerHMM(TestCase):
+class TestProdigal(TestCase):
     def setUp(self):
         self.options = update_config(get_simple_options(genefinding,
-                                     ['--taxon', 'fungi',
-                                      '--genefinding-tool', 'glimmerhmm',
-                                      '--cpus', '1']))
-        self.data_location = get_full_path(__file__, "data")
+                                                        ['--genefinding-tool', 'prodigal',
+                                                         '--cpus', '1']))
 
     def tearDown(self):
         get_config().__dict__.clear()
 
-    def data_file(self, filename):
-        return os.path.join(self.data_location, filename)
-
-    def test_fumigatus_cluster(self):
-        record = parse_input_sequence(self.data_file('fumigatus.cluster1.fna'))[0]
+    def test_nisin(self):
+        record = parse_input_sequence(get_path_to_nisin_fasta())[0]
         assert record.get_feature_count() == 0
         record = pre_process_sequences([record], self.options, genefinding)[0]
-        assert record.get_feature_count() == 11
+        assert record.get_feature_count() == 12
         # and make sure they're all CDS features
-        assert len(record.get_cds_features()) == 11
+        assert len(record.get_cds_features()) == 12
