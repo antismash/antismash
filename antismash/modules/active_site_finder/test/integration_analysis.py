b4beb695416e2474d6531342282f61a39e6c2f71
Simon Shaw <SJShaw@users.noreply.github.com>
2018-04-05 17:29:57 +0200

    active_site_finder: fix result regeneration

--- antismash/modules/active_site_finder/test/integration_analysis.py
+++ antismash/modules/active_site_finder/test/integration_analysis.py
@@ -8,16 +8,24 @@ import unittest
 
 import antismash
 from antismash.config import build_config
+from antismash.common import secmet
 from antismash.common.record_processing import parse_input_sequence
 from antismash.common.test import helpers
 from antismash.modules import active_site_finder
 
 class TestAnalyses(unittest.TestCase):
     def setUp(self):
-        self.options = build_config(["--asf", "--clusterhmmer", "--minimal"],
+        # skipping clusterhmmer and the p450 potential hits for speed
+        self.options = build_config(["--asf", "--minimal"],
                             isolated=True,
                             modules=antismash.get_all_modules())
 
-    def test_everything(self):
+    def test_regeneration(self):
         datafile = helpers.get_path_to_balhymicin_genbank()
         results = helpers.run_and_regenerate_results_for_module(datafile, active_site_finder, self.options)
+        assert results.pairings
+        for domain, labels in results.pairings:
+            for label in labels:
+                assert label
+                assert isinstance(label, str)
+            assert isinstance(domain, secmet.AntismashDomain)
