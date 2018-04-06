a4770f29a35fb7ef163363062404193fb90ff511
Simon Shaw <SJShaw@users.noreply.github.com>
2018-04-06 13:54:46 +0200

    active_site_finder: add additional tests

--- antismash/modules/active_site_finder/test/test_analysis.py
+++ antismash/modules/active_site_finder/test/test_analysis.py
@@ -4,13 +4,17 @@
 # for test files, silence irrelevant and noisy pylint warnings
 # pylint: disable=no-self-use,protected-access,missing-docstring,invalid-name
 
+import string
 import unittest
+from unittest import mock as umock
 
 from Bio import SearchIO
 from minimock import mock, restore
 
 from antismash.common import subprocessing  # mocked, pylint: disable=unused-import
 from antismash.common import fasta, path, secmet
+from antismash.common.test.helpers import DummyRecord
+from antismash.modules import active_site_finder
 from antismash.modules.active_site_finder import analysis
 
 
@@ -31,6 +35,20 @@ def rebuild_domains(filename, domain_type):
         domain.translation = translation
         domains.append(domain)
     return domains
+
+
+class DummyAlignment:
+    def __init__(self, domain, positions, scaffold_match=True):
+        self.domain = domain
+        self.positions = positions
+        self.scaffold_match = scaffold_match
+
+    def extract_position(self, position: int) -> str:
+        print(position, self.positions)
+        return self.positions[position]
+
+    def extract_positions(self, positions):
+        return "".join([self.positions[position] for position in positions])
 
 
 class TestAnalyses(unittest.TestCase):
@@ -108,17 +126,6 @@ class TestAnalyses(unittest.TestCase):
                     'PKS_DH1': 'catalytic triade H,G,P inconclusive'}
         assert {dom.domain_id: message for dom, message in results} == expected
 
-    def test_KR_bug(self):
-        # currently breaks in BioPython
-        try:
-            parse_hmmpfam_results("KR.output")
-        except ValueError as err:
-            if "Sequence lengths do not match" not in str(err):
-                raise
-            self.skipTest("Biopython bug preventing real output check")
-        # and if it works, update the other KR tests to use KR.output instead of KR.output.altered
-        self.fail("Unexpected success parsing hmmpfam2 output, biopython bug probably fixed. Update this test.")
-
     def test_KR_stereo(self):
         # use an altered output to avoid the Biopython bug
         mock("subprocessing.run_hmmpfam2", returns=parse_hmmpfam_results("KR.output.altered"))
@@ -165,3 +172,120 @@ class TestAnalyses(unittest.TestCase):
         expected = {'PFAM_p450_0': 'active site cystein inconclusive',
                     'PFAM_p450_1': 'active site cystein inconclusive'}
         assert {dom.domain_id: message for dom, message in results} == expected
+
+
+class TestSynthetic(unittest.TestCase):
+    """ Covers those areas that the real inputs above miss """
+    def setUp(self):
+        self.record = DummyRecord()
+
+    def tearDown(self):
+        restore()
+
+    def test_kr_stereo(self):
+        for char in string.ascii_uppercase:
+            mock("active_site_finder.common.ActiveSiteAnalysis.get_alignments",
+                 returns=[DummyAlignment("match", {102: char})])
+            pairings = analysis.pksi_kr_stereo(self.record)
+            if char == "D":
+                assert pairings == [("match", "KR domain putatively catalyzing D-configuration product formation")]
+            else:
+                assert pairings == [("match", "KR domain putatively catalyzing L-configuration product formation")]
+
+    def test_acp_type(self):
+        for current in string.ascii_uppercase:
+            mock("active_site_finder.common.ActiveSiteAnalysis.get_alignments",
+                 returns=[DummyAlignment(current, {37: current})])
+            pairings = analysis.acp_type(self.record)
+            if current == "W":
+                assert pairings == [(current, "beta-branching ACP")]
+            else:
+                assert pairings == [(current, "non-beta-branching ACP")]
+
+    def test_pksi_dh(self):
+        with umock.patch.object(active_site_finder.common.ActiveSiteAnalysis, "scaffold_matches") as mocked_match:
+            mocked_match.return_value = True
+            for values in ["GGP", "HPP", "HGG", "NNN", "HGP"]:
+                mock("active_site_finder.common.ActiveSiteAnalysis.get_alignments",
+                     returns=[DummyAlignment(values, {5: values[0], 39: values[1], 44: values[2]})])
+                pairings = analysis.asp_pksi_dh(self.record)
+                assert pairings == [(values, "catalytic triade H,G,P found: %s" % (values == "HGP"))]
+            # and an inconclusive
+            mocked_match.return_value = False
+            # values here don't matter
+            mock("active_site_finder.common.ActiveSiteAnalysis.get_alignments",
+                 returns=[DummyAlignment("incon", {})])
+            pairings = analysis.asp_pksi_dh(self.record)
+            assert pairings == [("incon", "catalytic triade H,G,P inconclusive")]
+
+    def test_thioesterase(self):
+        with umock.patch.object(active_site_finder.common.ActiveSiteAnalysis, "scaffold_matches") as mocked_match:
+            mocked_match.return_value = True
+            for char in string.ascii_uppercase:
+                mock("active_site_finder.common.ActiveSiteAnalysis.get_alignments",
+                     returns=[DummyAlignment(char, {81: char})])
+                pairings = analysis.asp_thioesterase(self.record)
+                assert pairings == [(char, "active site serine present: %s" % (char == "S"))]
+            # and an inconclusive
+            mocked_match.return_value = False
+            # values here don't matter
+            mock("active_site_finder.common.ActiveSiteAnalysis.get_alignments",
+                 returns=[DummyAlignment("incon", {})])
+            pairings = analysis.asp_thioesterase(self.record)
+            assert pairings == [("incon", "active site serine inconclusive")]
+
+    def test_er_stereo(self):
+        with umock.patch.object(active_site_finder.common.ActiveSiteAnalysis, "scaffold_matches") as mocked_match:
+            mocked_match.return_value = True
+            for char in string.ascii_uppercase:
+                mock("active_site_finder.common.ActiveSiteAnalysis.get_alignments",
+                     returns=[DummyAlignment(char, {39: char})])
+                pairings = analysis.pksi_er_stereo(self.record)
+                if char == "Y":
+                    assert pairings == [(char, "ER domain putatively catalyzing 2S-configuration product formation")]
+                else:
+                    assert pairings == [(char, "ER domain putatively catalyzing 2R-configuration product formation")]
+            # and an inconclusive
+            mocked_match.return_value = False
+            # values here don't matter
+            mock("active_site_finder.common.ActiveSiteAnalysis.get_alignments",
+                 returns=[DummyAlignment("incon", {})])
+            pairings = analysis.pksi_er_stereo(self.record)
+            assert pairings == [("incon", "ER configuration inconclusive")]
+
+    def test_at_spec(self):
+        with umock.patch.object(active_site_finder.common.ActiveSiteAnalysis, "scaffold_matches") as mocked_match:
+            mocked_match.return_value = True
+            for sig in ["HF", "YS", "HS", "YF", "NN"]:
+                mock("active_site_finder.common.ActiveSiteAnalysis.get_alignments",
+                     returns=[DummyAlignment(sig, {195: sig[0], 197: sig[1]})])
+                pairings = analysis.pksi_at_spec(self.record)
+                if sig == "HF":
+                    assert pairings == [(sig, "Malonyl-CoA specific")]
+                elif sig == "YS":
+                    assert pairings == [(sig, "Methylmalonyl-CoA specific")]
+                else:
+                    assert pairings == [(sig, "Neither malonyl-CoA or methylmalonyl-CoA specific")]
+            # and an inconclusive
+            mocked_match.return_value = False
+            # values here don't matter
+            mock("active_site_finder.common.ActiveSiteAnalysis.get_alignments",
+                 returns=[DummyAlignment("incon", {})])
+            pairings = analysis.pksi_at_spec(self.record)
+            assert pairings == [("incon", "(Methyl)Malonyl-CoA specificity inconclusive")]
+
+    def test_p450(self):
+        with umock.patch.object(active_site_finder.common.ActiveSiteAnalysis, "scaffold_matches") as mocked_match:
+            mocked_match.return_value = True
+            for char in string.ascii_uppercase:
+                mock("active_site_finder.common.ActiveSiteAnalysis.get_alignments",
+                     returns=[DummyAlignment(char, {407: char})])
+                pairings = analysis.asp_p450_oxy(self.record)
+                assert pairings == [(char, "active site cystein present: %s" % (char == "C"))]
+            # and an inconclusive
+            mocked_match.return_value = False
+            # values here don't matter
+            mock("active_site_finder.common.ActiveSiteAnalysis.get_alignments",
+                 returns=[DummyAlignment("incon", {})])
+            pairings = analysis.asp_p450_oxy(self.record)
+            assert pairings == [("incon", "active site cystein inconclusive")]
