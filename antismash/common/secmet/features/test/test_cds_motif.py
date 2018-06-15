78cadf18eefe4668a3d1f0eefb0fbe23c5562b78
Simon Shaw <SJShaw@users.noreply.github.com>
2018-06-15 13:47:15 +0200

    secmet: add tests for CDSMotif

---
+++ antismash/common/secmet/features/test/test_cds_motif.py
@@ -0,0 +1,19 @@
+# License: GNU Affero General Public License v3 or later
+# A copy of GNU AGPL v3 should have been included in this software package in LICENSE.txt.
+
+# for test files, silence irrelevant and noisy pylint warnings
+# pylint: disable=no-self-use,protected-access,missing-docstring
+
+import unittest
+
+from antismash.common.secmet.features import CDSMotif, FeatureLocation
+
+class TestConversion(unittest.TestCase):
+    def test_motif_conversion(self):
+        original = CDSMotif(FeatureLocation(2, 5))
+        original.motif = "some motif info"
+
+        bio_features = original.to_biopython()
+        assert len(bio_features) == 1
+        new = CDSMotif.from_biopython(bio_features[0])
+        assert new.motif == original.motif == "some motif info"
