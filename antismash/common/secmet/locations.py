c14fb415f0e83117aef8e43ca857dc265e94e444
Nestor <nestor@linuxmail.org>
2018-10-04 14:59:29 +0200

    secmet: add merge_within_container() function
    returns a merged location based on two locations, but only when the merged location is contained in the reference location. The latter, the "reference" location is mostly what is returned from extend_location_by() function.

--- antismash/common/secmet/locations.py
+++ antismash/common/secmet/locations.py
@@ -245,6 +245,19 @@ def extend_location_by(location: FeatureLocation, by: int, record: SeqRecord) ->
     return CompoundLocation(parts) if len(parts) > 1 else parts[0]
 
 
+def merge_within_container(first: FeatureLocation, second: FeatureLocation, container: FeatureLocation) -> FeatureLocation:
+    """ Merges two locations, but only if the merge location would still be fully contained in a container location
+        Returns:
+            a new, merged FeatureLocation, which is similar to FeatureLocation(min(first.start, second.start),max(first.end, second.end)),
+            but allows for cross-ori locations, both as first, second or container
+    """
+    parts = []
+    for container_subpart in container.parts: #only one for most cases, two for ori-split
+        contained_parts = [part for part in first.parts + second.parts if locations_overlap(container_subpart, part)]
+        parts.append(FeatureLocation(min(part.start for part in contained_parts), max(part.end for part in contained_parts)))
+
+    return CompoundLocation(parts) if len(parts) > 1 else parts[0]
+
 
 def combine_locations(*locations: Iterable[FeatureLocation]) -> FeatureLocation:
     """ Combines multiple FeatureLocations into a single location using the
