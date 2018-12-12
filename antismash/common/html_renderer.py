1bc70492d4402df13b77d772ad4f803d3e80d3c6
Simon Shaw <SJShaw@users.noreply.github.com>
2018-12-12 14:41:50 +0100

    common.html_renderer: use Markup instead of strings to allow autoescaping

--- antismash/common/html_renderer.py
+++ antismash/common/html_renderer.py
@@ -9,8 +9,25 @@ import os
 from typing import Any, Optional
 
 import jinja2 as _jinja2
+from jinja2 import Markup
 
-def collapser_start(target: str, level: str = "all") -> str:
+def collapser_start(target: str, level: str = "all") -> Markup:
+    """ Builds the start of a collapser specific to the target. Must be matched
+        with a collapser_end() call.
+
+        Arguments:
+            target: the name of the target (e.g. CDS name)
+            level: the interaction level at which this collapser will be expanded,
+                   possible levels are:
+                all: always expanded, must be manually collapsed
+                supercluster: expands when the relevant supercluster is selected
+                cluster: expands when the relevant cluster is selected
+                cds: expands when the relevant CDS is selected
+                none: never expands automatically, must be manually expanded
+
+        Returns:
+            HTML fragments as a Markup instance
+    """
     if level not in ["all", "supercluster", "cluster", "cds", "none"]:
         raise ValueError("unknown collapser level: %s" % level)
     classes = ["collapser", "collapser-target-%s" % target]
@@ -19,11 +36,13 @@ def collapser_start(target: str, level: str = "all") -> str:
     if level == "all":
         classes.append("expanded")
         child = child[:-1] + ' style="display:block;">'
-    return '<div class="%s"> </div>%s' % (" ".join(classes), child)
+    return Markup('<div class="%s"> </div>%s' % (" ".join(classes), child))
 
 
-def collapser_end() -> str:
-    return "</div>"
+def collapser_end() -> Markup:
+    """ Builds the end of a collapser, must be matched with a collapser_start() call.
+    """
+    return Markup("</div>")
 
 
 class _Template:
@@ -37,7 +56,7 @@ class _Template:
             loader = _jinja2.BaseLoader()
         else:
             loader = _jinja2.FileSystemLoader(template_dir)
-        self.env = _jinja2.Environment(loader=loader, autoescape=False,
+        self.env = _jinja2.Environment(loader=loader, autoescape=True,
                                        undefined=_jinja2.StrictUndefined)
 
     def render(self, **kwargs: Any) -> str:
