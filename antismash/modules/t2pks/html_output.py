e90ec60b4df72fc95b989667f3ca9763d3a953bd
Simon Shaw <SJShaw@users.noreply.github.com>
2019-01-31 17:20:12 +0100

    t2pks: add a basic help tooltip

--- antismash/modules/t2pks/html_output.py
+++ antismash/modules/t2pks/html_output.py
@@ -18,7 +18,7 @@ def will_handle(products: List[str]) -> bool:
 
 
 def generate_html(region_layer: RegionLayer, results: T2PKSResults,
-                  _record_layer: RecordLayer, _options_layer: OptionsLayer) -> HTMLSections:
+                  _record_layer: RecordLayer, options_layer: OptionsLayer) -> HTMLSections:
     """ Generate the sidepanel HTML with results from the type II PKS module """
     html = HTMLSections("t2pks")
 
@@ -28,6 +28,15 @@ def generate_html(region_layer: RegionLayer, results: T2PKSResults,
             predictions.append(results.cluster_predictions[cluster.get_cluster_number()])
 
     template = FileTemplate(path.get_full_path(__file__, "templates", "sidepanel.html"))
-    html.add_sidepanel_section("Type II PKS", template.render(predictions=predictions))
+
+    docs_url = options_layer.urls.docs_baseurl + "modules/t2pks"
+    tooltip_content = ("Predictions of starter units, elongations, product classes, "
+                       "and potential molecular weights for type II PKS clusters."
+                      )
+
+    tooltip_content += "<br>More detailed information is available <a href='%s' target='_blank'>here</a>." % docs_url
+
+    html.add_sidepanel_section("Type II PKS", template.render(predictions=predictions,
+                                                              tooltip_content=tooltip_content))
 
     return html
