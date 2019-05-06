fe938adf08297ad3c378bc52825a0d9dff431786
Kai Blin <kblin@biosustain.dtu.dk>
2019-05-06 11:00:12 +0200

    hmmdetection: Add cluster rule for thioamide-containing NRPs
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- antismash/detection/hmm_detection/test/test_hmm_detection.py
+++ antismash/detection/hmm_detection/test/test_hmm_detection.py
@@ -169,7 +169,7 @@ class HmmDetectionTest(unittest.TestCase):
 
     def test_profiles_parsing(self):
         profiles = signatures.get_signature_profiles()
-        assert len(profiles) == 241  # ensures we don't delete any by accident
+        assert len(profiles) == 243  # ensures we don't delete any by accident
 
     def test_filter(self):
         # fake HSPs all in one CDS with overlap > 20 and query_ids from the same equivalence group
