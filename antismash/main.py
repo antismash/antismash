3a166c1c79ee53311add0c0d02e33170d33fadaa
Kai Blin <kblin@biosustain.dtu.dk>
2020-02-19 13:30:33 +0100

    chore: bump version to 5.1.2
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- antismash/main.py
+++ antismash/main.py
@@ -56,7 +56,7 @@ from antismash.modules import (active_site_finder,
 from antismash.outputs import html, svg
 from antismash.custom_typing import AntismashModule
 
-__version__ = "5.1.1"
+__version__ = "5.1.2"
 
 
 def get_all_modules() -> List[AntismashModule]:
