55e87d402cbcaba9aabaa357e26eef9d078148e7
Simon Shaw <SJShaw@users.noreply.github.com>
2020-10-25 19:37:18 +0100

    common.hmmer: filter overlapping hits by default

--- antismash/common/test/test_hmmer.py
+++ antismash/common/test/test_hmmer.py
@@ -4,11 +4,12 @@
 # for test files, silence irrelevant and noisy pylint warnings
 # pylint: disable=no-self-use,protected-access,missing-docstring
 
-from dataclasses import FrozenInstanceError
+from collections import defaultdict
+from dataclasses import FrozenInstanceError, dataclass
 import json
 import unittest
 
-from antismash.common.hmmer import HmmerHit
+from antismash.common.hmmer import HmmerHit, remove_overlapping
 
 
 def create_hmmer_hit(location="[500:700]", label="ref_name", locus_tag="locus",
@@ -68,3 +69,81 @@ class TestHmmerHit(unittest.TestCase):
         hit = create_hmmer_hit()
         as_json = json.loads(json.dumps(hit.to_json()))
         assert hit == HmmerHit.from_json(as_json)
+
+
+class TestOverlaps(unittest.TestCase):
+    @dataclass(frozen=True)
+    class Dummy:
+        identifier: str
+        protein_start: int
+        protein_end: int
+        score: float = 10.
+
+        def __repr__(self):
+            # useful when tests fail
+            return self.identifier  # pragma: no cover
+
+        def __len__(self):
+            return self.protein_end - self.protein_start
+
+    def setUp(self):
+        self.cutoffs = defaultdict(lambda: 5.)
+        # these names match the effective scores, not the cutoff themselves
+        self.cutoffs.update({"low": 10., "med": 5., "high": 1.})
+
+    def test_no_overlap(self):
+        hit_a = self.Dummy("A", 50, 100)
+        hit_b = self.Dummy("B", 150, 200)
+        assert remove_overlapping([hit_a, hit_b], self.cutoffs) == [hit_a, hit_b]
+        assert remove_overlapping([hit_b, hit_a], self.cutoffs) == [hit_a, hit_b]
+
+    def test_limit(self):
+        hit_a = self.Dummy("A", 50, 200)
+        hit_b = self.Dummy("B", 150, 200)
+        assert len(remove_overlapping([hit_a, hit_b], self.cutoffs, overlap_limit=100)) == 2
+        assert len(remove_overlapping([hit_a, hit_b], self.cutoffs, overlap_limit=10)) == 1
+
+    def test_single_overlap(self):
+        hit_a = self.Dummy("low", 50, 150)
+        hit_b = self.Dummy("high", 100, 200)
+        assert remove_overlapping([hit_a, hit_b], self.cutoffs, overlap_limit=10) == [hit_b]
+        assert remove_overlapping([hit_b, hit_a], self.cutoffs, overlap_limit=10) == [hit_b]
+
+    def test_multiple_rising(self):
+        low = self.Dummy("low", 50, 150)
+        med = self.Dummy("med", 120, 220)
+        high = self.Dummy("high", 200, 300)
+        assert remove_overlapping([low, med, high], self.cutoffs, overlap_limit=10) == [low, high]
+
+    def test_multiple_falling(self):
+        high = self.Dummy("high", 50, 150)
+        med = self.Dummy("med", 120, 220)
+        low = self.Dummy("low", 200, 300)
+        assert remove_overlapping([low, med, high], self.cutoffs, overlap_limit=10) == [high, low]
+
+    def test_troughs(self):
+        first = self.Dummy("high", 50, 150)
+        mid = self.Dummy("med", 120, 220)
+        last = self.Dummy("high", 200, 300)
+        assert remove_overlapping([first, mid, last], self.cutoffs, overlap_limit=10) == [first, last]
+
+    def test_peaks(self):
+        first = self.Dummy("low", 50, 150)
+        mid = self.Dummy("med", 120, 220)
+        last = self.Dummy("low", 200, 300)
+        assert remove_overlapping([first, mid, last], self.cutoffs, overlap_limit=10) == [mid]
+
+    def test_tiebreaker_length(self):
+        hit_a = self.Dummy("a", 50, 125)
+        hit_b = self.Dummy("b", 100, 200)
+        # should take the longest in case of hit_a tie
+        assert remove_overlapping([hit_a, hit_b], self.cutoffs, overlap_limit=10) == [hit_b]
+        hit_a = self.Dummy("a", 50, 175)
+        assert remove_overlapping([hit_a, hit_b], self.cutoffs, overlap_limit=10) == [hit_a]
+
+    def test_tiebreaker_start(self):
+        a = self.Dummy("a", 50, 150)
+        b = self.Dummy("b", 100, 200)
+        # should take the longest in case of a tie
+        assert remove_overlapping([a, b], self.cutoffs, overlap_limit=10) == [a]
+        assert remove_overlapping([b, a], self.cutoffs, overlap_limit=10) == [a]
