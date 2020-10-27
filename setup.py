458ff2c96de3d931f58ca1729fcc7d37ba1f0271
Simon Shaw <SJShaw@users.noreply.github.com>
2020-10-27 10:01:59 +0100

    chore: bump minimum python version to 3.7

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ class PyTest(TestCommand):
 
 setup(
     name="antismash",
-    python_requires='>=3.5',
+    python_requires='>=3.7',
     version=read_version(),
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     package_data={
