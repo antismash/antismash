5b9b0feeeaa49adc45f54c5a349c08740320b755
Kai Blin <kblin@biosustain.dtu.dk>
2020-12-13 22:19:22 +0100

    tigrfam: Add new TIGRFam detection module
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

---
+++ antismash/detection/tigrfam/__init__.py
@@ -0,0 +1,90 @@
+# License: GNU Affero General Public License v3 or later
+# A copy of GNU AGPL v3 should have been included in this software package in LICENSE.txt.
+
+""" TIGRFam anotation for only clusters """
+
+import logging
+import os
+from typing import Any, Dict, List, Optional
+
+from antismash.common import hmmer, path
+from antismash.common.secmet import Record
+from antismash.config import ConfigType
+from antismash.config.args import ModuleArgs
+
+from .tigr_results import TIGRFamResults
+
+NAME = "tigrfam"
+SHORT_DESCRIPTION = "Cluster TIGRFam anotation"
+
+MIN_SCORE = 0.
+MAX_EVALUE = 0.01
+
+def get_arguments() -> ModuleArgs:
+    """ Builds the module args """
+    args = ModuleArgs('TIGRFam options', 'tigrfam')
+    args.add_analysis_toggle('tigrfam',
+                             dest='tigrfam',
+                             action='store_true',
+                             default=False,
+                             help="Annotate clusters using TIGRFam profiles.")
+    return args
+
+
+def is_enabled(options: ConfigType) -> bool:
+    """  Uses the supplied options to determine if the module should be run """
+    return options.tigrfam
+
+
+def check_prereqs(options: ConfigType) -> List[str]:
+    """ Ensure at least one database exists and is valid """
+    failure_messages = []
+    for binary_name in ['hmmscan']:
+        if binary_name not in options.executables:
+            failure_messages.append(f"Failed to locate executable: {binary_name!r}")
+
+    tigr_db = os.path.join(options.database_dir, "tigrfam", "TIGRFam.hmm")
+    if not path.locate_file(tigr_db):
+        failure_messages.append(f"Failed to locate TIGRFam db in {os.path.join(options.database_dir, 'tigrfam')}")
+
+    failure_messages.extend(hmmer.ensure_database_pressed(tigr_db, return_not_raise=True))
+
+    return failure_messages
+
+
+def check_options(_options: ConfigType) -> List[str]:
+    """ Check the requested PFAM database exists """
+    # No options to check
+    return []
+
+
+def regenerate_previous_results(previous: Dict[str, Any], record: Record,
+                                _options: ConfigType) -> Optional[TIGRFamResults]:
+    """ Rebuild previous results """
+    if not previous:
+        return None
+    results = TIGRFamResults.from_json(previous, record)
+    if not results:
+        return None
+    if results.score > MIN_SCORE or results.evalue < MAX_EVALUE:
+        # new values too lenient, discard resuts
+        return None
+    return results.refilter(MAX_EVALUE, MIN_SCORE)
+
+
+def run_on_record(record: Record, results: Optional[TIGRFamResults],
+                  options: ConfigType) -> TIGRFamResults:
+    """ Run hmmsearch against TIGRFam for all CDS features within the record """
+
+    logging.info('Running TIGRFam search')
+
+    if results:
+        return results
+
+    features = []
+    for region in record.get_regions():
+        features.extend(list(region.cds_children))
+    tigr_db = os.path.join(options.database_dir, "tigrfam", "TIGRFam.hmm")
+    hmmer_results = hmmer.run_hmmer(record, features, MAX_EVALUE, MIN_SCORE, tigr_db,
+                                    "tigrfam", filter_overlapping=False)
+    return TIGRFamResults.from_hmmer_results(hmmer_results)
---
+++ antismash/detection/tigrfam/test/integration_tigrfam.py
@@ -0,0 +1,93 @@
+# License: GNU Affero General Public License v3 or later
+# A copy of GNU AGPL v3 should have been included in this software package in LICENSE.txt.
+
+# for test files, silence irrelevant and noisy pylint warnings
+# pylint: disable=no-self-use,protected-access,missing-docstring
+
+import unittest
+
+import antismash
+from antismash.common import record_processing, pfamdb
+from antismash.common.test import helpers
+from antismash.config import build_config, destroy_config
+from antismash.detection import tigrfam
+
+
+class TestTIGRFam(unittest.TestCase):
+    def setUp(self):
+        self.original_min_score = tigrfam.MIN_SCORE
+        self.original_max_evalue = tigrfam.MAX_EVALUE
+        self.options = build_config(["--tigrfam", "--minimal"],
+                                    isolated=True,
+                                    modules=antismash.get_all_modules())
+
+    def tearDown(self):
+        self.set_max_evalue(self.original_max_evalue)
+        self.set_min_score(self.original_min_score)
+        destroy_config()
+
+    def set_max_evalue(self, evalue):
+        tigrfam.MAX_EVALUE = evalue
+
+    def set_min_score(self, score):
+        tigrfam.MIN_SCORE = score
+
+    def check_add_to_record(self, input_file, results):
+        record = record_processing.parse_input_sequence(input_file)[0]
+        assert not record.get_antismash_domains_by_tool("tigrfam")
+        results.add_to_record(record)
+        assert len(record.get_antismash_domains_by_tool("tigrfam")) == len(results.hits)
+
+    def test_reuse(self):
+        nisin = helpers.get_path_to_nisin_genbank()
+        record = record_processing.parse_input_sequence(nisin)[0]
+
+        results = helpers.run_and_regenerate_results_for_module(nisin, tigrfam, self.options)
+        json = results.to_json()
+        assert len(results.hits) == 2
+        self.check_add_to_record(nisin, results)
+
+        # test regeneration when thresholds are less restrictive
+        new_score_threshold = self.original_min_score - .1
+        self.set_min_score(new_score_threshold)
+        new_results = tigrfam.regenerate_previous_results(json, record, self.options)
+        assert new_results is None
+        self.set_min_score(self.original_min_score)
+
+        new_evalue_threshold = self.original_max_evalue + .1
+        self.set_max_evalue(new_evalue_threshold)
+        new_results = tigrfam.regenerate_previous_results(json, record, self.options)
+        assert new_results is None
+        self.set_max_evalue(self.original_max_evalue)
+
+        # test regeneration when evalue threshold is more restrictive
+        new_evalue_threshold = sorted(hit.evalue for hit in results.hits)[0]
+        assert new_evalue_threshold < self.original_max_evalue
+        new_hits = []
+        for hit in results.hits:
+            if hit.evalue <= new_evalue_threshold:
+                new_hits.append(hit)
+        new_hits.sort(key=lambda x: x.evalue)
+        assert len(new_hits) == 1
+
+        self.set_max_evalue(new_evalue_threshold)
+        new_results = tigrfam.regenerate_previous_results(json, record, self.options)
+        self.set_max_evalue(self.original_max_evalue)
+        assert sorted(new_results.hits, key=lambda x: x.evalue) == new_hits
+        self.check_add_to_record(nisin, results)
+
+        # test regeneration when score threshold is more restrictive
+        new_score_threshold = sorted(hit.score for hit in results.hits)[1]
+        assert new_score_threshold > tigrfam.MIN_SCORE
+        new_hits = []
+        for hit in results.hits:
+            if hit.score >= new_score_threshold:
+                new_hits.append(hit)
+        new_hits.sort(key=lambda x: x.score)
+        assert len(new_hits) == 1
+
+        self.set_min_score(new_score_threshold)
+        new_results = tigrfam.regenerate_previous_results(json, record, self.options)
+        self.set_min_score(self.original_min_score)
+        assert sorted(new_results.hits, key=lambda x: x.score) == new_hits
+        self.check_add_to_record(nisin, results)
