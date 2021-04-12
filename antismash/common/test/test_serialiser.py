53582f887548beb9446fe4991fa3708e3232d75b
Simon Shaw <SJShaw@users.noreply.github.com>
2021-04-12 21:14:15 +0200

    common.serialiser: include summary of record areas in JSON

--- antismash/common/test/test_serialiser.py
+++ antismash/common/test/test_serialiser.py
@@ -14,7 +14,8 @@ from helperlibs.wrappers.io import TemporaryDirectory
 
 from antismash.common import serialiser
 from antismash.common.secmet import Record
-from antismash.common.test.helpers import get_path_to_nisin_genbank
+from antismash.common.secmet.test import helpers
+from antismash.common.test.helpers import DummyRecord, get_path_to_nisin_genbank
 
 
 class TestResultsJSON(unittest.TestCase):
@@ -94,3 +95,54 @@ class TestFeatureSerialiser(unittest.TestCase):
         assert new_feature.id == feature.id
         assert new_feature.type == feature.type
         assert str(new_feature.location) == str(new_feature.location)
+
+
+class TestAreas(unittest.TestCase):
+    def test_empty(self):
+        record = DummyRecord()
+        result = serialiser.gather_record_areas(record)
+        assert result == []
+
+    def test_complex(self):
+        protos = [
+            helpers.DummyProtocluster(core_start=3, core_end=22, neighbourhood_range=1, product='a'),
+            helpers.DummyProtocluster(core_start=33, core_end=48, neighbourhood_range=2, product='b'),
+        ]
+        candidates = [
+            helpers.DummyCandidateCluster(clusters=protos[:1]),
+            helpers.DummyCandidateCluster(clusters=protos, kind=helpers.DummyCandidateCluster.kinds.INTERLEAVED)
+        ]
+        subregion = helpers.DummySubRegion(start=55, end=90)
+        regions = [
+            helpers.DummyRegion(candidate_clusters=candidates, subregions=[]),
+            helpers.DummyRegion(candidate_clusters=[], subregions=[subregion]),
+        ]
+
+        record = DummyRecord(record_id="test", seq="A"*90,
+                             features=regions + [subregion] + candidates + protos)
+        results = serialiser.gather_record_areas(record)
+        assert results
+        assert len(results) == 2
+        res = results[0]
+        assert res["end"] == protos[1].location.end
+        assert res["products"] == [p.product for p in protos]
+        assert res["subregions"] == []
+        assert len(res["protoclusters"]) == 2
+        assert list(res["protoclusters"]) == [0, 1]
+        assert res["protoclusters"][1]["product"] == protos[1].product
+        assert len(res["candidates"]) == 2
+        assert res["candidates"][0]["protoclusters"] == [0]
+        assert res["candidates"][1]["protoclusters"] == [0, 1]
+
+        res = results[1]
+        assert res["start"] == subregion.location.start
+        assert res["subregions"]
+        assert res["subregions"][0]["start"] == subregion.location.start
+        assert res["subregions"][0]["tool"] == subregion.tool
+        assert res["subregions"][0]["label"] == subregion.label
+        assert not res["protoclusters"]
+
+        # lastly, check all this is properly embedded in the final JSON
+        bio = record.to_biopython()
+        full = serialiser.dump_records([bio], [{}], [record])
+        assert full[0]["areas"] == results
