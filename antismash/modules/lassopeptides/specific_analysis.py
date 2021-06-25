3a1726f4fc8a8efb762d7329168de1ec727bb9d4
Simon Shaw <SJShaw@users.noreply.github.com>
2021-06-25 18:49:25 +0200

    lassopeptides: lassopeptides: deconflict prepeptide feature name

--- antismash/modules/lassopeptides/specific_analysis.py
+++ antismash/modules/lassopeptides/specific_analysis.py
@@ -681,7 +681,9 @@ def result_vec_to_motif(query: CDSFeature, result: Lassopeptide) -> Prepeptide:
     cut_mass = result.cut_mass
     cut_weight = result.cut_weight
 
-    feature = Prepeptide(query.location, "lassopeptide", core, query.get_name(), "lassopeptides",
+    product = "lassopeptide"
+    name = f"{query.get_name()}_{product}"
+    feature = Prepeptide(query.location, product, core, name, "lassopeptides",
                          peptide_subclass=result.lasso_class,
                          score=result.score, monoisotopic_mass=result.monoisotopic_mass,
                          molecular_weight=weight,
