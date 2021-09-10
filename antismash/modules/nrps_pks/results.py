12d215719c646d250f9e075dca8595d0b5365a65
Simon Shaw <SJShaw@users.noreply.github.com>
2021-09-10 14:23:16 +0200

    nrps_pks: make result schema version public

--- antismash/modules/nrps_pks/results.py
+++ antismash/modules/nrps_pks/results.py
@@ -111,7 +111,7 @@ class CandidateClusterPrediction:
 
 class NRPS_PKS_Results(ModuleResults):
     """ The combined results of the nrps_pks module """
-    _schema_version = 3
+    schema_version = 3
     __slots__ = ["consensus", "consensus_transat", "region_predictions", "domain_predictions"]
 
     def __init__(self, record_id: str) -> None:
@@ -142,7 +142,7 @@ class NRPS_PKS_Results(ModuleResults):
         region_json = {}
         for region, preds in self.region_predictions.items():
             region_json[region] = [pred.to_json() for pred in preds]
-        results = {"schema_version": self._schema_version,
+        results = {"schema_version": self.schema_version,
                    "record_id": self.record_id,
                    "domain_predictions": domain_predictions,
                    "consensus": self.consensus,
@@ -154,7 +154,7 @@ class NRPS_PKS_Results(ModuleResults):
     @staticmethod
     def from_json(json: Dict[str, Any], _record: Record) -> Optional["NRPS_PKS_Results"]:
         assert "record_id" in json
-        if json.get("schema_version") != NRPS_PKS_Results._schema_version:
+        if json.get("schema_version") != NRPS_PKS_Results.schema_version:
             logging.warning("Mismatching schema version, dropping results")
             return None
         results = NRPS_PKS_Results(json["record_id"])
