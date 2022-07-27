a841fb747c736e3120d343ced360f3b0087c95df
Marios-Nikolaidis <marionik23@gmail.com>
2022-07-27 19:18:18 +0300

    Added muscle5 incompatibility error

--- antismash/common/subprocessing/muscle.py
+++ antismash/common/subprocessing/muscle.py
@@ -13,36 +13,45 @@ from .base import execute, get_config
 
 
 def run_muscle_single(seq_name: str, seq: str, comparison_file: str) -> Dict[str, str]:
-    """ Runs muscle over a single sequence against a comparison file in profile
-        mode and returns a dictionary of the resulting alignments
+    """Runs muscle over a single sequence against a comparison file in profile
+    mode and returns a dictionary of the resulting alignments
 
-        Arguments:
-            seq_name: the name of the query
-            seq: the sequence to align
-            comparison_file: the path of the file containing comparison sequences
+    Arguments:
+        seq_name: the name of the query
+        seq: the sequence to align
+        comparison_file: the path of the file containing comparison sequences
 
-        Returns:
-            a dictionary mapping sequence name (query or reference) to alignment
+    Returns:
+        a dictionary mapping sequence name (query or reference) to alignment
     """
     with NamedTemporaryFile(mode="w+") as temp_in:
         with NamedTemporaryFile(mode="w+") as temp_out:
             write_fasta([seq_name], [seq], temp_in.name)
             # Run muscle and collect sequence positions from file
-            result = execute([get_config().executables.muscle,
-                              "-profile", "-quiet",
-                              "-in1", comparison_file,
-                              "-in2", temp_in.name,
-                              "-out", temp_out.name])
+            result = execute(
+                [
+                    get_config().executables.muscle,
+                    "-profile",
+                    "-quiet",
+                    "-in1",
+                    comparison_file,
+                    "-in2",
+                    temp_in.name,
+                    "-out",
+                    temp_out.name,
+                ]
+            )
             if not result.successful():
-                raise RuntimeError("muscle returned %d: %r while comparing query named %s" % (
-                                   result.return_code, result.stderr.replace("\n", ""),
-                                   seq_name))
+                raise RuntimeError(
+                    "muscle returned %d: %r while comparing query named %s"
+                    % (result.return_code, result.stderr.replace("\n", ""), seq_name)
+                )
             fasta = read_fasta(temp_out.name)
     return fasta
 
 
 def run_muscle_version() -> str:
-    """ Get the version of the muscle binary """
+    """Get the version of the muscle binary"""
     muscle = get_config().executables.muscle
     command = [
         muscle,
@@ -54,4 +63,8 @@ def run_muscle_version() -> str:
         msg = "unexpected output from muscle: %s, check path"
         raise RuntimeError(msg % muscle)
     # get rid of the non-version stuff in the output
+
+    if "muscle 5" in version_string.lower():
+        msg = "incompatible muscle binary version (version5): %s"
+        raise RuntimeError(msg % muscle)
     return version_string.split()[1]
