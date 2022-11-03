0c1f566164d47f7eaa36b18a90606869b7d920df
Simon Shaw <SJShaw@users.noreply.github.com>
2022-11-03 16:53:27 +0100

    chore: update test linting exclusions for newer pylint versions

--- antismash/detection/cassis/test/test_cassis.py
+++ antismash/detection/cassis/test/test_cassis.py
@@ -4,7 +4,7 @@
 """Test suite for the cassis cluster detection plugin"""
 
 # for test files, silence irrelevant and noisy pylint warnings
-# pylint: disable=no-self-use,protected-access,missing-docstring,consider-using-with
+# pylint: disable=use-implicit-booleaness-not-comparison,protected-access,missing-docstring,consider-using-with
 
 from argparse import Namespace
 import os
