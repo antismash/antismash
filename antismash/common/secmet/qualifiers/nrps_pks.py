03bcf6c211e45b3541ce9fa29ce5b6a1526c0632
FriederikeBiermann <friederike@biermann-erfurt.de>
2022-11-03 11:18:54 +0100

    Revert "Removed ghost function"
    This reverts commit 56326dff1df18c7d6096e093c686e6bd607071d1.

--- antismash/common/secmet/qualifiers/nrps_pks.py
+++ antismash/common/secmet/qualifiers/nrps_pks.py
@@ -190,6 +190,8 @@ class NRPSPKSQualifier:
                 self.add_domain(domain, parts[5], sub, subsub)
             elif qualifier.startswith("subtype: "):
                 self.add_subtype(_parse_format(_SUBTYPE_FORMAT, qualifier)[0])
+            elif qualifier.startswith("subsubtype: "):
+                self.add_subsubtype(_parse_format(_SUBTYPE_FORMAT, qualifier)[0])
             elif qualifier.startswith("type: "):
                 self.type = _parse_format(_TYPE_FORMAT, qualifier)[0]
             else:
