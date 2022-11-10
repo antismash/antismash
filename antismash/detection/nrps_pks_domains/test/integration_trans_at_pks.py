54fec204a5a7ee8df0ebd0cb6d43a0d08b5acf99
FriederikeBiermann <friederike@biermann-erfurt.de>
2022-11-10 13:07:05 +0100

    fixed to pass test

--- antismash/detection/nrps_pks_domains/test/integration_trans_at_pks.py
+++ antismash/detection/nrps_pks_domains/test/integration_trans_at_pks.py
@@ -82,8 +82,8 @@ class IntegrationTRANSATPKS(unittest.TestCase):
                             'Trans-AT-KS', 'Trans-AT-KS', 'Trans-AT-KS', 'Trans-AT-KS',
                             'Trans-AT-KS', 'Modular-KS', 'Modular-KS']
 
-        ks_subsubtypes_true = ['ST', 'NON_ELONGATING_BETA_OH', 'DB', '',
-                               'AA', '', 'TRANS_AT_PKS', 'TRANS_AT_PKS', '',
+        ks_subsubtypes_true = ['ST', 'NON_ELONGATING_BETA_OH', 'DB', 'unknown variant',
+                               'AA', 'unknown variant', 'TRANS_AT_PKS', 'TRANS_AT_PKS', 'unknown variant',
                                'BETA_OH_KETO', 'NON_ELONGATING_BETA_OH', 'DB']
         ks_subsubtypes = []
         for cds, cds_result in results_domains.cds_results.items():
