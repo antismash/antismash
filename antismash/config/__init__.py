8b0d05130a814749c3633bb7221308289bf4822c
Kai Blin <kblin@biosustain.dtu.dk>
2022-11-23 08:25:48 +0000

    chore: Use ~/.antismash7.cfg as the user's config file
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- antismash/config/__init__.py
+++ antismash/config/__init__.py
@@ -24,7 +24,7 @@ from .loader import load_config_from_file
 
 from .executables import find_executable_path
 
-_USER_FILE_NAME = os.path.expanduser('~/.antismash6.cfg')
+_USER_FILE_NAME = os.path.expanduser('~/.antismash7.cfg')
 _INSTANCE_FILE_NAME = os.path.abspath(os.path.join(os.path.dirname(__file__), 'instance.cfg'))
 
 
