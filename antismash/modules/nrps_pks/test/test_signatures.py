159231c91aa414c8a15678e62f6a541331d0baca
Simon Shaw <SJShaw@users.noreply.github.com>
2022-12-05 15:05:58 +0100

    nrps_pks: use NRPSPredictor's HMM file instead of muscle alignment

--- antismash/modules/nrps_pks/test/test_signatures.py
+++ antismash/modules/nrps_pks/test/test_signatures.py
@@ -5,22 +5,40 @@
 # pylint: disable=use-implicit-booleaness-not-comparison,protected-access,missing-docstring
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
-from antismash.common import path, fasta, subprocessing
+from antismash.common import subprocessing
 from antismash.common.test.helpers import DummyAntismashDomain
 from antismash.modules.nrps_pks import signatures
 
 
 class TestAngstromGeneration(unittest.TestCase):
     def test_angstrom(self):
-        aligns = fasta.read_fasta(path.get_full_path(__file__, 'data', 'nrpspred_aligns.fasta'))
+        # bpsC from Y16952, balhimycin
+        profile = (
+            "KGVmveHrnvvnlvkwlneryflfgeeddllgesdrvLqfssAysFDaSvweifgaLLnGgtLViv"
+            "pkefsetrlDpeaLaalieregiTvlnltPsllnllldaaeeatpdfapedlssLrrvlvGGEaLs"
+            "pslarrlrerfpdragvrliNaYGPTEtTVcaTi"
+        )
+        query = (
+            "KGVAIPHGAVAGLAGDAG---WQIGPGD-------GVLMHAT-HVFDPSLYAMWVPLVSGAR-VLL"
+            "TEP---GVLDAAGVRQAVHR-GATFVHLTAGTFRALAETA--------PECFEGLVEIGTGGDVVP"
+            "LQSVENLRRAQ---PGLRVRNTYGPTETTLCATW"
+        )
         domain = DummyAntismashDomain(domain_id="query")
-        domain.translation = aligns[domain.domain_id].replace("-", "")
-        with patch.object(subprocessing, "run_muscle_single", return_value=aligns):
-            sig_10aa, sig_34aa  = signatures.get_a_dom_signatures(domain)
-        assert sig_10aa == "DAFYLGMMCK"
-        assert sig_34aa == "L--SFDASLFEMYLLTGGDRNMYGPTEATMCATW"
+        domain.translation = query.replace("-", "")
+        hits = [Mock()]
+        hsp = Mock()
+        hsp.aln = (Mock(), Mock())
+        hsp.aln[1].seq = profile
+        hsp.aln[0].seq = query
+        hsp.hit_start = 0
+        hits[0].hsps = [hsp]
+        with patch.object(subprocessing.hmmpfam, "run_hmmpfam2", return_value=hits) as patched:
+            sig_10aa, sig_34aa = signatures.get_a_dom_signatures(domain)
+            assert patched.called_once
+        assert sig_10aa == "DPYHGGTLCK"
+        assert sig_34aa == "LDAAFDPSLYAVHLGTGGDRNTYGPTETTLCATW"
 
 
 class TestHelpers(unittest.TestCase):
@@ -31,13 +49,3 @@ class TestHelpers(unittest.TestCase):
         assert not check("!LSFD")
         assert not check("LSFD-LL")
         assert not check("{LS")
-
-
-    def test_extract(self):
-        extract = signatures.extract
-        sequence = "MAGIC-HAT"
-
-        assert "MT" == extract(sequence, [0, 8])
-        assert "IC" == extract(sequence, [3, 5])
-        assert "CH" == extract(sequence, [4, 5])
-        assert "C-H" == extract(sequence, [4, 5, 6])
