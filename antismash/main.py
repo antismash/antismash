3e19fe1bbf0c956dce8fdb2c47f6a154d549fedd
Simon Shaw <SJShaw@users.noreply.github.com>
2023-03-01 15:32:55 +0100

    chore: bump version to 7.0.0beta2

--- antismash/main.py
+++ antismash/main.py
@@ -40,7 +40,7 @@ from antismash.outputs import html, svg
 from antismash.support import genefinding
 from antismash.custom_typing import AntismashModule
 
-__version__ = "7.0.0beta1"
+__version__ = "7.0.0beta2"
 
 
 def _gather_analysis_modules() -> List[AntismashModule]:
