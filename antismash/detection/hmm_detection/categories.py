68e183244a4cb840fd124ceaf678865f76b01887
Simon Shaw <SJShaw@users.noreply.github.com>
2023-08-02 15:54:33 +0200

    hmm_detection: allow categories to have parent categories

--- antismash/detection/hmm_detection/categories.py
+++ antismash/detection/hmm_detection/categories.py
@@ -6,7 +6,7 @@
 
 from dataclasses import dataclass
 import json
-from typing import Dict, List, Type, TypeVar, Union
+from typing import Any, Dict, List, Type, Optional, TypeVar, Union
 
 from antismash.common import path
 
@@ -19,10 +19,12 @@ class RuleCategory:
     name: str
     description: str
     version: int = 1
+    parent: Optional["RuleCategory"] = None
 
     @classmethod
     def from_json(cls: Type[TRuleCategory], name: str,
-                  entry: Dict[str, Union[str, int]]) -> TRuleCategory:
+                  entry: Dict[str, Union[str, int]],
+                  existing_categories: dict[str, TRuleCategory]) -> TRuleCategory:
         """Regenerates a RuleCategory instance from a JSON representation"""
 
         try:
@@ -31,10 +33,36 @@ class RuleCategory:
         except KeyError as err:
             raise ValueError(f"missing required rule metadata key '{err}'")
 
+        parent: Optional[RuleCategory] = None
+        if "parent" in entry:
+            parent = existing_categories[str(entry["parent"])]
+
         if version != 1:
             raise ValueError(f"Unknown rule category version {version}")
 
-        return cls(name, description, version)
+        return cls(name, description, version, parent=parent)
+
+
+def _parse_categories(data: dict[str, Any]) -> dict[str, RuleCategory]:
+    # split entries into two sets, those without parents, then those with parents
+    top_level = {}
+    children = {}
+    for name, metadata in data.items():
+        if "parent" in metadata:
+            if metadata["parent"] not in data:
+                raise ValueError(f"category {name!r} refers to unknown category {metadata['parent']!r}")
+            children[name] = metadata
+        else:
+            top_level[name] = metadata
+
+    categories: dict[str, RuleCategory] = {}
+
+    # run on the top level first, to ensure that they exist to be referenced
+    for group in [top_level, children]:
+        for name, metadata in group.items():
+            categories[name] = RuleCategory.from_json(name, metadata, existing_categories=categories)
+
+    return categories
 
 
 def get_rule_categories() -> List[RuleCategory]:
@@ -48,13 +76,12 @@ def get_rule_categories() -> List[RuleCategory]:
         return existing
 
     # not cached, generate
-    categories: List[RuleCategory] = []
+    category_mapping: dict[str, RuleCategory] = {}
 
     with open(path.get_full_path(__file__, "data", "categories.json"), "r", encoding="utf-8") as handle:
-        category_json = json.load(handle)
-        for name, metadata in category_json.items():
-            categories.append(RuleCategory.from_json(name, metadata))
+        category_mapping = _parse_categories(json.load(handle))
 
+    categories = list(category_mapping.values())
     # and cache for future calls, and silence mypy warning as mypy can't handle this
     get_rule_categories.existing = categories  # type: ignore
 
