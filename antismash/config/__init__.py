8f78d50eb688fe7f7f2f39559ed129751ec525e0
Simon Shaw <SJShaw@users.noreply.github.com>
2023-08-18 16:36:54 +0200

    config: remove unused import

--- antismash/config/__init__.py
+++ antismash/config/__init__.py
@@ -22,8 +22,6 @@ from antismash.custom_typing import AntismashModule, ConfigType
 from .args import build_parser, AntismashParser
 from .loader import load_config_from_file
 
-from .executables import find_executable_path
-
 _USER_FILE_NAME = os.path.expanduser('~/.antismash7.cfg')
 _INSTANCE_FILE_NAME = os.path.abspath(os.path.join(os.path.dirname(__file__), 'instance.cfg'))
 
