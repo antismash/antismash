fc7412e32ef8d9f4b992676ed0c8d6ae87abaaa1
Simon Shaw <SJShaw@users.noreply.github.com>
2023-11-17 17:45:06 +0100

    secmet: fix format strings missing format marker

--- antismash/common/secmet/locations.py
+++ antismash/common/secmet/locations.py
@@ -402,7 +402,7 @@ def ensure_valid_locations(features: List[SeqFeature], can_be_circular: bool, se
             raise ValueError("one or more features with missing or invalid locations")
         # features outside the sequence cause problems with motifs and translations
         if feature.location.end > sequence_length:
-            raise ValueError("feature outside record sequence: {feature.location}")
+            raise ValueError(f"feature outside record sequence: {feature.location}")
         # features with overlapping exons cause translation problems
         if location_contains_overlapping_exons(feature.location):
             raise ValueError(f"location contains overlapping exons: {feature.location}")
