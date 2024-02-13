a99be846edd2c0cc839d323b92669121ad264855
Simon Shaw <SJShaw@users.noreply.github.com>
2024-02-13 16:59:44 +0100

    chore: gracefully exit from SIGINT without stack traces

--- run_antismash.py
+++ run_antismash.py
@@ -2,8 +2,13 @@
 """
 Convenience wrapper to run antiSMASH directly from the source tree without installing.
 """
-
-from antismash.__main__ import entrypoint
+import sys
+# wrap the import in a try/except as the import is lengthy and SIGINT is noisy
+# when it happens during the import process
+try:
+    from antismash.__main__ import entrypoint
+except KeyboardInterrupt:
+    sys.exit(2)
 
 if __name__ == '__main__':
-    entrypoint()
+    entrypoint()  # already wrapped in a graceful SIGINT try/except
