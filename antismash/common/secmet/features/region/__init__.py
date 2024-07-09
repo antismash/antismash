8343f496c5364b11ce9d30fa11d2c8baea02c025
Simon Shaw <SJShaw@users.noreply.github.com>
2024-07-09 14:07:09 +0200

    secmet: split region file into sections

---
+++ antismash/common/secmet/features/region/__init__.py
@@ -0,0 +1,8 @@
+# License: GNU Affero General Public License v3 or later
+# A copy of GNU AGPL v3 should have been included in this software package in LICENSE.txt.
+
+""" Contains a feature covering a region, along with helper/utility functions for
+    manipulating those features.
+"""
+
+from .structures import Region
