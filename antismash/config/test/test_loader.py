6e0dc7902abd0a47649c867af8546969eb79a23b
Simon Shaw <SJShaw@users.noreply.github.com>
2024-08-09 13:07:22 +0200

    config: allow default config file paths to be overridden

--- antismash/config/test/test_loader.py
+++ antismash/config/test/test_loader.py
@@ -5,6 +5,7 @@
 # pylint: disable=use-implicit-booleaness-not-comparison,protected-access,missing-docstring
 
 import os
+from tempfile import NamedTemporaryFile
 import unittest
 
 from antismash.config import loader
@@ -24,3 +25,14 @@ class TestLoader(unittest.TestCase):
         print(loader.load_config_from_file().__dict__)
         assert loader.load_config_from_file().base == "base_value"
         assert loader.load_config_from_file().child.base == "child_base_value"
+
+    def test_alternate(self):
+        assert not loader.load_config_from_file("/dev/null").__dict__
+        original_path = os.path.join(loader._BASEDIR, "default.cfg")
+        with NamedTemporaryFile() as altered:
+            with open(original_path, encoding="utf-8") as original:
+                content = original.read().replace("child_base_value", "altered_val")
+                altered.write(content.encode())
+            altered.seek(0)
+            config = loader.load_config_from_file(altered.name)
+        assert config.child.base == "altered_val"
