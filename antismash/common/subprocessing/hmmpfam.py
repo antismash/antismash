a47c1d21746f5bb4696ecc8f9c244610942deeb0
Simon Shaw <SJShaw@users.noreply.github.com>
2024-11-21 14:15:24 +0100

    common.subprocessing: use alignment extract from utils for hmmpfam

--- antismash/common/subprocessing/hmmpfam.py
+++ antismash/common/subprocessing/hmmpfam.py
@@ -6,7 +6,7 @@
 
 from io import StringIO
 import logging
-from typing import List
+from typing import List, Optional
 
 from .base import execute, get_config, SearchIO
 
@@ -14,7 +14,7 @@ _THREADING_SUPPORT = True
 
 
 def run_hmmpfam2(query_hmmfile: str, target_sequence: str, extra_args: List[str] = None
-                 ) -> list[SearchIO._model.query.QueryResult]:
+                 ) -> list[SearchIO.QueryResult]:
     """ Run hmmpfam2 over the provided HMM file and fasta input
 
         Arguments:
@@ -49,6 +49,31 @@ def run_hmmpfam2(query_hmmfile: str, target_sequence: str, extra_args: List[str]
         raise RuntimeError(f"hmmpfam2 problem while running {command}: {result.stderr}")
     res_stream = StringIO(result.stdout)
     return list(SearchIO.parse(res_stream, 'hmmer2-text'))
+
+
+def get_alignment_against_profile(sequence: str, db_path: str, profile_name: str, max_evalue: float = 0.1,
+                                  ) -> Optional[SearchIO.QueryResult]:
+    """ Aligns the given sequence against the given profile.
+
+        Arguments:
+            sequence: the protein sequence to align to the reference
+            db_path: the path of the profile to align the translation against
+            profile_name: the name of the specific profile within the database to align against
+            max_evalue: the maximum evalue threshold for any hits
+
+        Returns:
+            the first hit for the query against the named profile
+    """
+    args = ["-E", str(max_evalue)]
+    results = run_hmmpfam2(db_path, f">query\n{sequence}", extra_args=args)
+    if not (results and results[0].hsps):
+        return None
+
+    for hit in results[0].hsps:
+        if hit.hit_id == profile_name:
+            return hit
+
+    return None
 
 
 def run_hmmpfam2_help() -> str:
