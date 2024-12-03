02084b1c6af56db18fca4653d77c1147d7b1894e
Kai Blin <kblin@biosustain.dtu.dk>
2024-12-03 14:00:48 +0100

    chore: Bump version number to 8
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- antismash/main.py
+++ antismash/main.py
@@ -41,7 +41,7 @@ from antismash.outputs import html
 from antismash.support import genefinding
 from antismash.custom_typing import AntismashModule
 
-__version__ = "7.dev"
+__version__ = "8.dev"
 
 
 def _gather_analysis_modules() -> List[AntismashModule]:
